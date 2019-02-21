317442521195a11c2c81aaf3ee3c854e8e323383
qianying1 <1101627399@qq.com>
2019-02-21 22:48:46 +0800

    整理了注册中心的代码

--- src/main/java/cn/qianying/video/services/register/vsr/VsrApplication.java
+++ src/main/java/cn/qianying/video/services/register/vsr/VsrApplication.java
@@ -1,22 +1,23 @@
 package cn.qianying.video.services.register.vsr;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * 程序启动类
+ *
+ * @Author Nacht
+ * Created on 23/11/2018
+ */
 @EnableEurekaServer
 @SpringBootApplication
-public class VsrApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
-
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAutoConfiguration
+public class VsrApplication {
     public static void main(String[] args) {
-//        SpringApplication.run(VsrApplication.class, args);
-        new SpringApplicationBuilder(VsrApplication.class).run(args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return super.configure(builder);
+        SpringApplication.run(VsrApplication.class, args);
     }
 }
